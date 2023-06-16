18dbe45a48aada1ce499ab24d2ae0a3fcec6b738
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-16 17:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here
 
+function addTodo(){
+    let input = document.getElementById("newTodoInput");
+    let ol = document.getElementById("todoList");
+    let error = document.getElementById("error");
+
+    
+    if(input.value === ''){
+        error.innerText = "Please enter valid Task";
+    }
+
+    document.getElementById("newTodoInput").addEventListener("focus", (event) => {
+        error.innerText = "";
+    });
+
+    if(input.value !== ''){
+        const li = document.createElement("li");
+        li.innerText = input.value;
+        ol.appendChild(li);
+        input.value = "";
+
+
+    }
+
+
+
+    
+}
+
